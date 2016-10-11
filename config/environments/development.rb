486df992eb3e04350cfc5c23b2c56f99013e3912
caveonow <caveon_5759@hotmail.com>
2016-10-12 01:00:57 +0800

    update mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,16 +59,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      'app57772936@heroku.com',
-    password:       'fe6967j50597',
-    domain:         'heroku.com',
-    address:       'smtp.sendgrid.net',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
 
 end
