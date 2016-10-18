d5eadbf6e13844ec5e1283324d72cd8c2861656f
caveonow <caveon_5759@hotmail.com>
2016-10-18 22:57:03 +0800

    update report

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -11,24 +11,45 @@ $(function(){
   });
 
   // text size title
-  $("#sizetitle").change(function() {
+  $("#sizetitle").on("change paste keyup",function() {
 	 $('.changeMe').css("font-size", $(this).val() + "px");
   });
 
   // text size subtitle
-  $("#sizesubtitle").change(function() {
+  $("#sizesubtitle").on("change paste keyup",function() {
 	 $('.changeMe1').css("font-size", $(this).val() + "px");
   });	
 
-  // bold text title	
-  $("#boldtitle").click(function(){
-	$('.changeMe').css("font-weight","Bold");
+  // bold text title
+  $("#boldtitle").click(function() {
+    $(".changeMe").toggleClass("bold");
   });
-  
+
   // bold text subtitle
-  $("#boldsubtitle").click(function(){
-	$('.changeMe1').css("font-weight","Bold");
+  $("#boldsubtitle").click(function() {
+    $(".changeMe1").toggleClass("bold");
   });
+	
+  // italic text title
+  $("#italictitle").click(function() {
+    $(".changeMe").toggleClass("italic");
+  });
+
+  // italic text subtitle
+  $("#italicsubtitle").click(function() {
+    $(".changeMe1").toggleClass("italic");
+  });
+	
+  // underline text title
+  $("#undertitle").click(function() {
+    $(".changeMe").toggleClass("underline");
+  });
+
+  // underline text subtitle
+  $("#undersubtitle").click(function() {
+    $(".changeMe1").toggleClass("underline");
+  });	
+	
 	
 });
 
