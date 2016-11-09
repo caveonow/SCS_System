45556465240c0226b45aff8040c460f704fc1452
Nick <cool_nickro@hotmail.com>
2016-11-09 08:35:35 +0800

    minor change

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -73,7 +73,7 @@ class ReportsController < ApplicationController
     @formTry = Form.joins(:answer)
     @section = Section.joins(:form)
     @answer = Answer.all
-    render json: @formTry.group(:QuestionDesc).group(:AnswerDesc).where("IsSubAnswer = 0 and IsSubQuestion = 0 and section_id = 2").sum(:AnswerCount).chart_json
+    #render json: @formTry.group(:QuestionDesc).group(:AnswerDesc).where("IsSubAnswer = 0 and IsSubQuestion = 0 and section_id = 2").sum(:AnswerCount).chart_json
 
     render json: @user.group(:facultyname).where(:faculty_id =>['2','3','4','5','6']).count.chart_json
     #render json: @user.group(:gender).count.chart_json
